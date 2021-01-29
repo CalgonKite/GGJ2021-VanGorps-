122c9f3f00cdb150c565090b9ae4c4276b17940f
CalgonKite <CalgonKite@users.noreply.github.com>
2021-01-29 19:36:08 +0000

    player movment
    left and right movement, no jump, physics material added

---
+++ UnityProject/2D/2D-GGJ2021/Assets/Scripts/Movement/playerMove.cs
@@ -0,0 +1,102 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMove : MonoBehaviour
+{
+    //simple
+    public float speed;
+    //scalar
+    public float movement_scalar;
+    public float max_speed;
+    public float scalar_jump;
+    //components
+    private Rigidbody2D rigidbody2D;
+    public bool isground;
+    /// <summary>
+    /// mechanics for movement
+    /// Jump
+    /// Wall jump (instant) - use small trigger
+    /// Dash - Once in air
+    /// </summary>
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        //Move();
+        scalarMove();
+    }
+
+    private void Update()
+    {
+
+        /// jumping ///
+        /*if (isground == true && Input.GetAxis.name("Jump"))
+        {
+            Vector2 jumpvel = new Vector2(0, scalar_jump);
+            rigidbody2D.AddForce(jumpvel);
+        }
+        else
+        {
+
+        }*/
+    }
+
+    public void Move()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector2 moving = new Vector2(moveHorizontal, moveVertical);
+        rigidbody2D.AddForce(moving * speed);
+    }
+
+    //handles inputs for movement
+    public void scalarMove()
+    {
+        /// movement code ///
+        float movehoriz = Input.GetAxis("Horizontal");
+        //ADD A GROUNDED BOOL
+        if (rigidbody2D.velocity.magnitude < max_speed)
+        {
+            Vector2 moving = new Vector2(movehoriz, 0);
+            rigidbody2D.AddForce(movement_scalar * moving);
+        }
+        //Vector2 moving = new Vector2(movehoriz, 0);
+        //rigidbody2D.AddForce(movement_scalar * moving);
+
+        /// jumping ///
+        /*if (isground == true && Input.GetButtonDown("Jump"))
+        {
+            Vector2 jumpvel = new Vector2(0, scalar_jump);
+            rigidbody2D.AddForce(jumpvel);
+        }
+        else
+        {
+
+        }*/
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Floor")
+        {
+            isground = true;
+            Debug.Log("touching ground");
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Floor")
+        {
+            isground = false;
+            Debug.Log("Left ground");
+        }
+    }
+}
